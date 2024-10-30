1bd8ba6b75e21f66ccf52d5baf008bb813c31f45
manamisinc2016 <161755382+manamisinc2016@users.noreply.github.com>
2024-10-29 21:35:23 -0500

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,15 @@
+def send_email (message, recipient, *, sender = "university.help@gmail.com"):
+    allowed_domains = (".com" , ".ru", ".net")
+    if "@" not in sender or not sender.endswith(allowed_domains) or "@" not in recipient or not recipient.endswith(allowed_domains):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
+    elif sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+    elif sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+    else:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+
+send_email('Это сообщение для проверки связи', 'Anastasiasal@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
